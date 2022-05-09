10a59032156eff5332a3ccc1072cf30c7e36ff0e
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2022-05-09 10:15:54 -0700

    created the joke controller class. closes #4

---
+++ src/main/java/app/jokesapp/controllers/JokesController.java
@@ -0,0 +1,25 @@
+package app.jokesapp.controllers;
+
+import app.jokesapp.services.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokesService jokesService;
+
+    // @Autowired not needed b/c there is only one constructor
+    @Autowired
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokesService.getJoke());
+        return "index";
+    }
+}
